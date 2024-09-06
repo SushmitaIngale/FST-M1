2c1e2ae9885a6a15f8e421282af06a22ef7d7cc2
SushmitaIngale <87584778+SushmitaIngale@users.noreply.github.com>
2024-09-06 12:16:53 +0530

    Add files via upload

---
+++ Java/Activities/Activity5.java
@@ -0,0 +1,9 @@
+package src.activities;
+
+public class Activity5 {
+    public static void main(String[] args) {
+        Book newNovel = new MyBook();
+        newNovel.setTitle("Harry Potter and the Philosopher's Stone");
+        System.out.println(newNovel.getTitle());
+    }
+}
